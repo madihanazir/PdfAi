6bf3ca33fe4eec351c521b34921516ae3d4cb120
Madiha Nazir <125150474+madihanazir@users.noreply.github.com>
2025-02-25 17:04:52 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pinkBg: "#ffdde1",
+        pinkAccent: "#ff69b4",
+      },
+      boxShadow: {
+        kawaii: "5px 5px 15px rgba(255, 182, 193, 0.5)",
+      },
+    },
   },
   plugins: [],
 };
