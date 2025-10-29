402260326f6662f7b7884e9653152e5b10902524
Madiha Nazir <125150474+madihanazir@users.noreply.github.com>
2025-10-29 19:18:20 +0530

    Use environment variable for API base URL

--- frontend/src/components/UploadPDF.jsx
+++ frontend/src/components/UploadPDF.jsx
@@ -11,7 +11,9 @@ function UploadPDF({ onFileUpload }) {
             formData.append("file", file);
 
             try {
-                const res = await axios.post("http://127.0.0.1:8000/documents/upload", formData);
+               const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+                const res = await axios.post(`${API_BASE_URL}/documents/upload`, formData);
+
                 setMessage("File uploaded successfully!");
                 onFileUpload(res.data.document_id, file.name);
             } catch (error) {
